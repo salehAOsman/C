33765740625a997728f85ed32ced1ebfb48c0a52
Saleh Osman <saleh1.a.osman@gmail.com>
2018-01-11 11:58:36 +0100

    Add project files.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Computeages = new List<int> { 2, 4, 6, 8, 10, 12 };
+            void CheckAges()
+            {
+                foreach (int item in Computeages)
+                {
+                    if (item == 10) return;
+                }
+            }
+        }
+    }
+}
---
+++ StringRead/StringRead/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringRead
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frstStr   = "The quick fox Jumped Over the DOG";
+            string secondStr = "The brown fox jumped over the lazy dog";
+            Console.WriteLine("Input new string :");
+            string inStr= Console.ReadLine();
+            ComparStr(inStr);
+        }
+        static void ComparStr(string inStr)
+        {
+            //string inString = instr;
+            string lastInWord = "";
+            string thred = "        ";
+
+            foreach (int item in inStr)
+                {
+                   // lastInWord = instr[item];
+                    if (inStr[item].ToString() != lastInWord)
+                    {
+                        thred.Insert(item, inStr[item].ToString());
+                    }
+                else
+                {
+                    Console.WriteLine("there is error");
+                }
+                }
+            
+            Console.WriteLine("first word is : "+ thred);
+            Console.ReadKey();
+
+        }
+    }
+}
